759b1e4a6df0c65f0b0ae560b067cf459a29a488
kumaravish847@gmail.com <kumaravish847@gmail.com>
2026-07-02 12:12:25 +0530

    edit

--- apps/excelidraw-frontend/draw/http.ts
+++ apps/excelidraw-frontend/draw/http.ts
@@ -2,7 +2,7 @@ import { HTTP_BACKEND } from "@/config";
 import axios from "axios";
 
 export async function getExistingShapes(roomId: string) {
-    const res = await axios.get(`${HTTP_BACKEND}/chats/${roomId}`);
+    const res = await axios.get(`${HTTP_BACKEND}/canvas/${roomId}`);
     const messages = res.data.messages;
 
     const shapes = messages.map((x: {message: string}) => {
